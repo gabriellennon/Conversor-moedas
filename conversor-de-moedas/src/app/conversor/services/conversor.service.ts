69ce86680628ae29295498f2ce81679284f0a3d6
Gabriel Lennon <gabriellennon@wizsolucoes.com.br>
2021-03-01 16:32:33 -0300

    servicos

---
+++ conversor-de-moedas/src/app/conversor/services/conversor.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+//servico responsável por consultar a API
+export class ConversorService {
+
+  constructor() { }
+}
