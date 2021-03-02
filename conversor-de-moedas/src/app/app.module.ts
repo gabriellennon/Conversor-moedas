24295b60135ee7b81dbd88ab657ce20858ea278f
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-03-02 19:31:33 -0300

    teste

--- conversor-de-moedas/src/app/app.module.ts
+++ conversor-de-moedas/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { from } from 'rxjs';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ConversorModule } from './conversor/conversor.module';
@@ -11,6 +13,8 @@ import { ConversorModule } from './conversor/conversor.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     ConversorModule
   ],
   providers: [],
